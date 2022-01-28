62563b5470604e6c744bf87463237a42f7c6de45
Vlad Zusko <vladzvu11@gmail.com>
2022-01-29 00:50:56 +0200

    feat: added basic reducers tests

---
+++ src/store/trending/index.test.ts
@@ -0,0 +1,42 @@
+import reducer from './index';
+import { initialState } from './state';
+import * as actions from './actions';
+
+describe('test trending reducer', () => {
+  it('test initializing reducer', () => {
+    expect(reducer(undefined, { type: null })).toEqual(initialState);
+  });
+
+  it('test set trending action', () => {
+    expect(reducer(undefined, actions.setTrending({ tiktuks: [] }))).toEqual({
+      ...initialState,
+      items: [],
+    });
+  });
+
+  it('test set page action', () => {
+    expect(reducer(undefined, actions.setPage({ page: 1 }))).toEqual({
+      ...initialState,
+      options: {
+        ...initialState.options,
+        pageNumber: 1,
+      },
+    });
+  });
+
+  it('test set loading action', () => {
+    expect(reducer(undefined, actions.setLoading({ isLoading: true }))).toEqual(
+      {
+        ...initialState,
+        isLoading: true,
+      },
+    );
+  });
+
+  it('test set error action', () => {
+    expect(reducer(undefined, actions.setError({ isError: true }))).toEqual({
+      ...initialState,
+      isError: true,
+    });
+  });
+});
